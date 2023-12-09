a32aa656c5f99ffae9fbd130d67bc44b1f4906ac
mzlin2020 <mzlin2020@163.com>
2023-12-09 10:23:46 +0800

    全局配置调整

--- src/lib/AntdRegistry.tsx
+++ src/lib/AntdRegistry.tsx
@@ -7,7 +7,12 @@ import React from "react";
 
 const StyledComponentsRegistry = ({ children }: React.PropsWithChildren) => {
   const cache = React.useMemo<Entity>(() => createCache(), []);
-  useServerInsertedHTML(() => <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />);
+  const isServerInserted = React.useRef<boolean>(false);
+  useServerInsertedHTML(() => {
+    if (isServerInserted.current) return;
+    isServerInserted.current = true;
+    return <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />;
+  });
   return <StyleProvider cache={cache}>{children}</StyleProvider>;
 };
 
